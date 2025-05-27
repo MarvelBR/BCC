3bca884e15c83598cee747626c5b414c6138fb70
MarvelBR <erickmg2005@gmail.com>
2025-05-27 15:25:59 -0300

    .

--- 3ºSemestre/Algoritmos e Estrutura de Dados 1/Prova2/Codigos/teste.cpp
+++ 3ºSemestre/Algoritmos e Estrutura de Dados 1/Prova2/Codigos/teste.cpp
@@ -22,17 +22,14 @@ void bubbleSort(std::vector<int>& v) {
 void gerar_e_testar(int tamanho, const std::string& nome_arquivo) {
     std::vector<int> dados(tamanho);
 
-    std::mt19937 rng(std::random_device{}());
-    std::uniform_int_distribution<int> dist(0, 1000000);
+    srand(time(0)); // Semente
 
-    for (int& x : dados)
-        x = dist(rng);
-
-    // Garante que a pasta "dados/" existe
-    std::filesystem::create_directories("dados");
+    for (int& x : dados){
+        x = rand() % 1000000;
+    }
 
     // Escreve os dados originais (desordenados) no arquivo
-    std::ofstream arquivo("dados/" + nome_arquivo, std::ios::binary);
+    std::ofstream arquivo("../Dados/" + nome_arquivo, std::ios::binary);
     if (!arquivo) {
         std::cerr << "Erro ao criar arquivo dados/" << nome_arquivo << "\n";
         return;
@@ -59,7 +56,7 @@ int main() {
     // Estime os tamanhos com base em testes anteriores
     int pequeno = 10000;     // Alvo: ~1 segundo
     int medio   = 50000;     // Alvo: ~30 segundos
-    int grande  = 130000;     // Alvo: ~180 segundos
+    int grande  = 120000;     // Alvo: ~180 segundos
 
     gerar_e_testar(pequeno, "pequeno.bin");
     gerar_e_testar(medio,   "medio.bin");
