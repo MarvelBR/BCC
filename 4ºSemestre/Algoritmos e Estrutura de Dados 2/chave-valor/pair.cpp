a71ab6c41c89213691a305233162bec94fb2f663
MarvelBR <erickmg2005@gmail.com>
2025-09-29 21:01:23 -0300

    🤫🤫🤫🤫🤫

--- 4ºSemestre/Algoritmos e Estrutura de Dados 2/chave-valor/pair.cpp
+++ 4ºSemestre/Algoritmos e Estrutura de Dados 2/chave-valor/pair.cpp
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
+#include <utility>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,7 +22,19 @@ std::vector<int> gerar_vetor_aleatorio(int n, int seed, int max){
 }
 
 pair <int,int> twosum(vector<int> & v, int k){
-    
+    unordered_set<int> d;
+    for(int x: v){
+        d.insert(x);
+    }
+    for (int x: v)
+    {
+        if (d.count(k-x) == 1)
+        {
+            return make_pair(x, k-x);
+        }
+        
+    }
+    return make_pair(-1,-1);
 }
 
 int main(int argc, char** argv){
