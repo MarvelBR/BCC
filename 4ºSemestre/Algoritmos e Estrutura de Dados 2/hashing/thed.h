1a07670a83490fe05815b2f7267d6c598d7ad73d
MarvelBR <erickmg2005@gmail.com>
2025-10-09 17:14:35 -0300

    .

--- 4ºSemestre/Algoritmos e Estrutura de Dados 2/hashing/thed.h
+++ 4ºSemestre/Algoritmos e Estrutura de Dados 2/hashing/thed.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <utility>
+#include <iostream>
 
 class TabHashEncadeamento {
 public:
@@ -18,32 +19,67 @@ public:
     //Destrutor: libera todos os recursos alocados para a tabela
     ~TabHashEncadeamento(){
         delete[] this->tabela;
-    } 
+    }
     
     //Insere um novo par (chave, valor) na tabela
     void inserir(int chave, int valor){
         auto& el = this->buscar(chave);
-        if (el != this->invalido)
-        {
+        if(el != this->invalido){
             //chave já está na tabela
             el.second = valor;
             return;
         }
+
+        int fator_carga = this->n / this->m;
+
+        if (fator_carga >= this->limiar)
+        {
+            this->redimensionar(this->m * 2);
+        }
+
         int h = hash(chave);
         this->tabela[h].push_back(std::make_pair(chave, valor));
+        this->n++;
     }
     
     //Remove o par com a chave da tabeka
     void remover(int chave);
     
     //Busca o valor associado a chave na tabela
-    std::pair<int,int>& buscar(int chave);
+    std::pair<int,int>& buscar(int chave){
+        int h = hash(chave);
+        for (auto& el : this->tabela[h])
+        {
+            if (el.first == chave)
+            {
+                return el;
+            }
+            
+        }
+        return this->invalido;
+    }
     
     //Imprime a tabela
-    void imprimir();
+    void imprimir(){
+        for (int i = 0; i < this->m; i++)
+        {
+            std::cout << i << ": ";
+            for (auto& el : this->tabela[i])
+            {
+                std::cout << "(" << el.first << "," << el.second << "), ";
+            }
+            std::cout << "\n";
+        }
+        
+    }
 
     //Imprime informações sobre a tabela (m, n e fator de carga)
-    void imprimir_info();
+    void imprimir_info(){
+        std::cout << "M = " << this->m << ", n = " << this->n 
+        << ", fator_carga = " << (float) this->n / this->m
+        << "\nredims = " << this->redims
+        << "\n";
+    }
 
     //par chave-valor inválido para indicar que a chave não foi encontrada
     std::pair<int,int> invalido;
@@ -62,11 +98,31 @@ private:
     int redims; // número de redimensionamentos realizados
 
     std::vector<std::pair<int,int>> *tabela; // tabela hash
+    
     int hash(int chave){
         // função hash
         return chave % this->m;
-    }
+    } 
+
     // redimensiona a tabela para o novo tamanho (novo_m)
-    void redimensionar(int novo_m); 
+    void redimensionar(int novo_m){
+        auto* antiga = this->tabela;
+        int m_antigo = this->m;
+        this->n = 0;
+        this->tabela = new std::vector<std::pair<int,int>>[novo_m];
+        for (int i = 0; i < m_antigo; i++)
+        {
+            if (antiga[i].size() > 0)
+            {
+                for (auto& el : antiga[i])
+                {
+                    this->inserir(el.first, el.second);
+                }
+                
+            }
+            
+        }
+        delete[] antiga;
+    } 
     
 };
